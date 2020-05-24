d2865109feabde0ee9edb6c06daa8f4a91b6ea35
conao3 <conao3@gmail.com>
2020-05-25 08:43:53 +0900

    generate project

---
+++ peek.el
@@ -0,0 +1,42 @@
+;;; peek.el --- Show peek window for grep  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; Version: 0.0.1
+;; Keywords: convenience
+;; Package-Requires: ((emacs "26.1"))
+;; URL: https://github.com/conao3/peek.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Show peek window for grep.
+
+
+;;; Code:
+
+(defgroup peek nil
+  "Show peek window for grep."
+  :group 'convenience
+  :link '(url-link :tag "Github" "https://github.com/conao3/peek.el"))
+
+(provide 'peek)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+
+;;; peek.el ends here
